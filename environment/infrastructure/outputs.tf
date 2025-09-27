74e67e4bcbb8de50ebb433aad81d61cde014ac6c
Marcin Franczyk <marcin0franczyk@gmail.com>
2025-09-27 13:03:41 +0200

    fix

--- environment/infrastructure/outputs.tf
+++ environment/infrastructure/outputs.tf
@@ -1,9 +1,11 @@
 output "network_name" {
   value = local.vpc_name
+  sensitive = true
 }
 
 output "subnet_name" {
   value = module.vpc.subnets["${local.region}/${local.private_subnet_name}"].name
+  sensitive = true
 }
 
 output "service_account_email" {
